8aa3d9a87d8f99205b85079c47d621541c603b4f
MrLazy100 <prempatil98@gmail.com>
2018-07-03 11:32:28 +0530

    first commit

---
+++ set_py2.py
@@ -0,0 +1,20 @@
+A=set('Dipalee')
+B=set('Dips')
+print('A is :',A)
+
+print('length of A is :',len(A))
+
+print('B is :',B)
+
+print('length of B is :',len(B))
+
+print('Union is :',A|B)
+
+print('Intersection is :',A&B)
+
+print('Difference is from A:',A-B)
+
+print('Elements is a and b but not in both :',A^B)
+
+print('Difference from B is :',B-A)
+
